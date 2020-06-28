f6b702c1fd4c84ccd2309e41298226a0e2ce363c
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2020-06-28 16:59:39 -0300

    mudanças layout 0.3

--- chamado_todos.php
+++ chamado_todos.php
@@ -13,35 +13,38 @@ $chamados = $chamadoDAO->listarTodos($funcionario);
 <div class="content-wrapper">
   	<div class="container-fluid" style="margin-top: 30px;">
 		  <div class="d_flex"></div>
-			<div class="container">
+			<div class="container" style="margin-bottom: 180px;">
 				<div class="h3topo" style="text-align: center;">
         			<h3>Lista de Chamados</h3>
     			</div><br>
 				<div class="row">
 				<?php foreach ($chamados as $chamado) { ?>
-					<div class="col-sm-6">
+					<div class="col-sm-6" style="padding-bottom: 15px;">
 						<div class="card">
 							<div class="card-body">
 								<h5 class="card-title">Chamado n° <?= $chamado->getid() ?></h5>
-								<ul>
-									<li>
+								<ul class="list-group" style="padding-bottom: 15px; padding-top: 15px;">
+									<li class="list-group-item">
 										Cliente: <strong><?= $chamado->nome_cliente; ?></strong>
 									</li>
-									<li>
+									<li class="list-group-item">
 										Funcionário: <strong><?= $chamado->nome_funcionario; ?></strong>
 									</li>
-									<li>
+									<li class="list-group-item">
 										Status: <strong><?= $chamado->getStatus() ?></strong>
 									</li>
-									<li>
+									<li class="list-group-item">
 										Equipamento: <strong><?= $chamado->getEquipamento() ?></strong>
 									</li>
-									<li>
+									<li class="list-group-item">
 										Abertura: <strong><?= $chamado->getAbertura() ?></strong>
+									</li>
+									<li class="list-group-item">
+										Descrição: <strong><?= $chamado->getDescricao() ?></strong>
 									</li>
 								</ul>
 								<a target="__blank" href="pdf_chamado_finalizado.php?id=<?= $chamado->getId() ?>" class="btn btn-success"><i class="fas fa-file-pdf"></i></a>
-								<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#descricaodotick">Ver descrição</button>
+								<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#descricaodotick">Histórico</button>
 							</div>
 						</div>
 					</div>
@@ -73,8 +76,23 @@ $chamados = $chamadoDAO->listarTodos($funcionario);
 		</div>
 	</div>
 <?php } ?>
-
-
+<!-- <?php if (isset($_GET['id']) && $_GET['id'] != '') { ?>
+        <div class="col-md-6">
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Registrar Historico</button>
+          <tbody class="table-striped">
+		  <?php foreach ($historicoChamados as $historicoChamado) { ?>
+              <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Funcionario: <?= $historicoChamado->nome; ?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Data: <?= $historicoChamado->getDtHistorico() ?></h6>
+                  <p class="card-text"><?= $historicoChamado->getDescricao() ?></p>
+                  <p class="card-text"><?= $historicoChamado->getSolucao() ?></p>
+                </div>
+              </div>
+            <?php } ?>
+          </tbody>
+        </div>
+      <?php } ?> -->
 
 
 <?php include 'layout/footer.php'; ?>
